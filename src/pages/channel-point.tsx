fe9f78fb1b5c9dd9db7833d51f84d18969e308d3
LoukHin <loukhin3141@gmail.com>
2021-08-15 14:19:33 +0700

    :lipstick: - apply layout to pages

---
+++ src/pages/channel-point.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import Head from 'components/head'
+
+const ChannelPoints: NextPage = (props) => {
+    return (
+        <>
+            <Head title='Channel Point'></Head>
+            Channel point
+        </>
+    )
+}
+
+export default ChannelPoints
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,11 @@
 import { NextPage } from 'next'
-import { useRouter } from 'next/router'
 import Head from 'components/head'
-import Button from 'components/button'
-import TopNav from 'components/topnav'
-import { signOut, useSession } from 'next-auth/client'
-import { useEffect } from 'react'
 
 const Index: NextPage = (props) => {
-    const [session, loading] = useSession()
-    const router = useRouter()
-
-    useEffect(() => {
-        if (!loading && !session) router.push('/auth/signin')
-    }, [session, loading])
-
     return (
         <>
-            <Head title='test'></Head>
-            <TopNav></TopNav>
-            {session?.user?.image ? <img src={session.user.image} /> : ''}
-            Signed in as {JSON.stringify(session?.user?.name)} <br />
-            <Button
-                onClick={() => signOut()}
-                className='self-center text-white text-lg duration-200 bg-twitch-purple hover:bg-twitch-purple-dark'
-            >
-                Sign out
-            </Button>
-
-            <div className="h-80"></div>
-            <div className="h-80"></div>
-            <div className="h-80"></div>
-            <div className="h-80"></div>
-            <div className="h-80"></div>
+            <Head></Head>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum quisquam aspernatur, nisi soluta enim debitis voluptates fugit consectetur iusto error maiores velit at excepturi, laboriosam ipsa recusandae, sit quasi voluptate?
         </>
     )
 }
