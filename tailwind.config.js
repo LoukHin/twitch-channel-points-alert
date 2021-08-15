33b1cd09cd94385432fca30fc7fd45fe05a5e8ae
LoukHin <loukhin3141@gmail.com>
2021-08-15 12:29:02 +0700

    :bug: - update purgecss rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    purge: ['src/pages/**/*.{js,ts,jsx,tsx}', 'src/components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false,
     theme: {
         extend: {
